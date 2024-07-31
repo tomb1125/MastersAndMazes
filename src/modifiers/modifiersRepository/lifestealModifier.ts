c73e6b1b03b45d0966021cac4b8348553fc4cb4c
Tomasz Bogdański <tomasz.bogdanski@gmail.com>
2024-07-31 21:17:28 +0200

    wip

--- src/modifiers/modifiersRepository/lifestealModifier.ts
+++ src/modifiers/modifiersRepository/lifestealModifier.ts
@@ -1,5 +1,4 @@
 import { DescriptiveNumberFactory } from "../../components/descriptiveNumberFactory";
-import { CharacterContext } from "../../core/characterContext";
 import { PowerModifier } from "../../core/powerModifier";
 import { Modifier } from "../modifier";
 
@@ -7,13 +6,13 @@ export class lifestealModifier extends Modifier {
     
     constructor() {
         super();
-        this.weight = 1000; 
+        this.weight = 1; 
         this.type = Modifier.Type.Improvement;
         this.name = 'Lifesteal'; 
         this.numericComponents = DescriptiveNumberFactory.get(1);
         this.namePrefix = 'Leeching'; 
         this.description = 'When you hit, heal yourself equal to damage taken by enemy.';
-        this.powerMultiplier = (x: PowerModifier) => {return 0.55}; //TODO this should be bonus euqal to damage... however damage is set after modifiers...s
+        this.powerMultiplier = (x: PowerModifier) => {return 0.55}; //TODO this should be bonus equal to damage... however damage is set after modifiers...s
         this.longDescription = '';
     }
 }
